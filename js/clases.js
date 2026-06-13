d8f7a85833c3365653552d58e1022f1d3806b9bf
Belen <belro0419@gmail.com>
2026-06-13 17:08:30 -0300

    cambiando el html e iniciando el js

---
+++ js/clases.js
@@ -0,0 +1,17 @@
+// Ana Belen Rodriguez Berriel / N° de estudiante: 373879 -  Iván De León Lino / N° de estudiante: 330339
+
+class Sistema {
+  constructor() {}
+}
+
+class Influencer {
+  constructor() {}
+}
+
+class Articulo {
+  constructor() {}
+}
+
+class Venta {
+  constructor() {}
+}
---
+++ js/funciones.js
@@ -0,0 +1,10 @@
+// TODO: Hacer que el <button class="button articles__table-button">Código ↕</button> ordene la tabla de forma ascendente a descendete y visceversa.
+/* TODO: Investigar como hacer la grafica de burbujas.
+- Debe representar seis valores numericos
+- El equipo deberá investigar cómo generar la gráfica de burbujas de colores. Se debe mostrar una gráfica de burbujas que
+represente seis valores numéricos. Cada burbuja corresponde al total de ventas de ese medio y su tamaño es proporcional al
+monto representado: la burbuja asociada al valor mínimo debe tener un radio equivalente al 10 % del radio máximo,
+mientras que la burbuja asociada al valor máximo debe tener el 100 % del radio máximo.
+Las demás burbujas deben escalarse de forma proporcional entre esos extremos. Debe indicarse el monto de cada burbuja.
+Los colores son a elección. Deben quedar alineadas.
+*/
